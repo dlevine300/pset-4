517221df9fff5a3e1e1eb5e8b9effc27f15f8859
David Levine <55397183+dlevine300@users.noreply.github.com>
2019-11-21 21:44:31 -0500

    started prime

---
+++ src/prime.js
@@ -0,0 +1,23 @@
+const readlineSync = require("readline-sync");
+const MAX =Number.MAX_SAFE_INTEGER
+const MIN =0;
+
+let value = readlineSync.question("\nNon-Negative integer: ");
+let isPrime = value;
+
+while (value <= 0) {
+  value = readlineSync.question("Non-Negative integer: ");
+
+
+  for (var i = 2; i < value; i++) {
+    if (value % i == 0) {
+      isPrime = false;
+    } else
+    isPrime= true;
+  }
+}
+if (isPrime) {
+  console.log("\nPrime.");
+} else {
+  console.log("\nNot Prime.");
+}
