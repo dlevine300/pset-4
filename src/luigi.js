354d41f4209b47481eddb470cdccc85766f52974
David Levine <55397183+dlevine300@users.noreply.github.com>
2019-11-24 15:03:31 -0500

    Finished Luigi

--- src/luigi.js
+++ src/luigi.js
@@ -1,36 +1,30 @@
 const readlineSync = require('readline-sync');
-const MAX = 24;
-const MIN = 1;
-let height = readlineSync.question('\nHeight: ');
+
+let height = 0;
+
+console.log();
 
 while (height < 1 || height > 24) {
   height = readlineSync.question("Height: ");
 
-  height = parseInt(height);
+  value = parseInt(height);
 
-  if (height > 0 && height <= 24) {
+  if (value > 0 && value <= 24) {
 
-    for (i = 1; i <= height; i++) {
-      var spaces = height - i;
-      var hashes = i + 1;
+    for (i = 1; i <= value; i++) {
+    var spaces = value - i;
+    var hashtags = i + 1;
 
-      var space = ('');
-      var hashtag = ('');
-      var space1 = ('  ');
+    var space = '';
+    var hashtag = '';
 
-      for (j = 1; j <= spaces; j++) {
-        space += ' ';
-      }
-      for (j = 1; j <= hashes; j++) {
-        hashtag += '#';
-      }
-      for (var count = 2; count < 9; count++) {
-        let string = (new Array(count + 1).join('#'));
-        for (var space = 1; space > 0; space--) {
-          string1 = string + (new Array(space + 1).join(' '));
-                console.log(space + hashtag + space1 + string1);
-        }
-      }
+    for( j = 1; j <= spaces; j++) {
+    space += ' ';
     }
-  }
-}
+    for( j = 1; j <= hashtags; j++ ) {
+    hashtag += '#';
+    }
+    console.log(space + hashtag + " " + hashtag);
+    }
+    }
+    }
